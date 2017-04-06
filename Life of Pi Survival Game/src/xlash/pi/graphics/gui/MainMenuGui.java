b8ad085437af09281bf3aab6632e5ae6319d5a03
Noah <xlash19o13@gmail.com>
2017-04-05 22:34:32 -0400

    Tested with buttons.

--- Life of Pi Survival Game/src/xlash/pi/graphics/gui/MainMenuGui.java
+++ Life of Pi Survival Game/src/xlash/pi/graphics/gui/MainMenuGui.java
@@ -2,21 +2,39 @@ package xlash.pi.graphics.gui;
 
 import java.awt.Font;
 import java.awt.Graphics2D;
+import java.awt.Point;
+
+import xlash.pi.Game;
+import xlash.pi.util.GuiUtil;
 
 public class MainMenuGui extends Gui{
+	
+	public ButtonGui sampleButton;
 
 	public MainMenuGui() {
+		sampleButton = new ButtonGui(100,100,100,40,"Hi mom"){
+			@Override
+			public void onActivate() {
+				Game.window.toggleFullscreen();
+			}
+		};
 	}
 	
 	public void onWindowChange(int width, int height){
-		this.width = width;
-		this.height = height;
+		this.setPosition(getX(), getY(), width, height);
+	}
+	
+	@Override
+	public void onClick(Point point, int button, boolean pressed){
+		this.sampleButton.onClick(point, button, pressed);
 	}
 
 	@Override
 	public void draw(Graphics2D g2d) {
 		g2d.setFont(new Font("DisposableDroid BB", Font.TRUETYPE_FONT, 50));
-		g2d.drawString("Life of Pi - Survival Game", 20, 100);
+		GuiUtil.drawStringCenteredWidth(g2d, "Life of Pi - Survival Game", bounds, 90);
+		g2d.setFont(new Font("DisposableDroid BB", Font.TRUETYPE_FONT, 32));
+		sampleButton.draw(g2d);
 	}
 
 }

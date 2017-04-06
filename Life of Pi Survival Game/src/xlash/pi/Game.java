069c52ca34c16c324201a675614ac5cc9e6cc853
Noah <xlash19o13@gmail.com>
2017-04-05 22:36:06 -0400

    Runs game loop and passes mouse clicks through gui

--- Life of Pi Survival Game/src/xlash/pi/Game.java
+++ Life of Pi Survival Game/src/xlash/pi/Game.java
@@ -4,76 +4,106 @@ import java.awt.Font;
 import java.awt.FontFormatException;
 import java.awt.Graphics2D;
 import java.awt.GraphicsEnvironment;
-import java.io.File;
+import java.awt.Point;
+import java.awt.event.MouseEvent;
 import java.io.IOException;
 import java.io.InputStream;
 
 import xlash.pi.graphics.Window;
 import xlash.pi.graphics.gui.Gui;
 import xlash.pi.graphics.gui.MainMenuGui;
+import xlash.pi.input.InputHandler;
 
 public class Game {
-	
+
 	public static Window window;
 	public Gui currentGui;
-	
-	public Game(){
-		window = new Window(){
+
+	public Game() {
+		window = new Window() {
 			@Override
-			public void draw(Graphics2D g2d){
+			public void draw(Graphics2D g2d) {
 				Game.this.draw(g2d);
 			}
 		};
-		Thread gameLoop = new Thread("Game Loop"){
+		Thread gameLoop = new Thread("Game Loop") {
 			@Override
-			public void run(){
+			public void run() {
+				System.out.println("Initializing...");
 				initialize();
+				System.out.println("Initializing done!");
+				gameLoop();
 			}
 		};
-		gameLoop.start();
-		Thread renderLoop = new Thread("Render Loop"){
+		Thread renderLoop = new Thread("Render Loop") {
 			@Override
-			public void run(){
+			public void run() {
 				renderLoop();
 			}
 		};
+		gameLoop.start();
 		renderLoop.start();
 	}
-	
-	public void initialize(){
-		try{
-			GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
-			InputStream fontStream = getClass().getClassLoader().getResourceAsStream("xlash/pi/resources/font/DisposableDroidBB.ttf");
+
+	public void initialize() {
+		try {
+			InputStream fontStream = getClass().getClassLoader()
+					.getResourceAsStream("xlash/pi/resources/font/DisposableDroidBB.ttf");
 			Font font = Font.createFont(Font.TRUETYPE_FONT, fontStream);
 			GraphicsEnvironment.getLocalGraphicsEnvironment().registerFont(font);
 			System.out.println("Registered font of name " + font.getName());
-		}catch (IOException|FontFormatException e){
+		} catch (IOException | FontFormatException e) {
 			e.printStackTrace();
 		}
 		this.currentGui = new MainMenuGui();
 	}
-	
-	public void gameLoop(){
-		
-	}
-	
-	public void renderLoop(){
-		while(true){
-			window.repaint();
+
+	public void gameLoop() {
+		while (true) {
+			InputHandler.tick();
+
+			Point mousePoint = window.getMousePoint();
+			if (mousePoint != null) {
+				if (InputHandler.isMouseButtonPressed(MouseEvent.BUTTON1)) {
+					this.currentGui.onClick(window.getMousePoint(), MouseEvent.BUTTON1, true);
+				} else if (InputHandler.didMouseUnclick(MouseEvent.BUTTON1)) {
+					this.currentGui.onClick(window.getMousePoint(), MouseEvent.BUTTON1, false);
+				}
+				if (InputHandler.isMouseButtonPressed(MouseEvent.BUTTON2)) {
+					this.currentGui.onClick(window.getMousePoint(), MouseEvent.BUTTON2, true);
+				} else if (InputHandler.didMouseUnclick(MouseEvent.BUTTON1)) {
+					this.currentGui.onClick(window.getMousePoint(), MouseEvent.BUTTON2, false);
+				}
+			}
+
+			InputHandler.untick();
 			try {
-				Thread.sleep(1000/60);
+				Thread.sleep(1000 / 60);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
 		}
 	}
-	
+
+	public void renderLoop() {
+		while (true) {
+			window.repaint();
+			try {
+				Thread.sleep(1000 / 60);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
 	/**
 	 * Draw method for the panel.
+	 * 
 	 * @param g2d
 	 */
-	public void draw(Graphics2D g2d){
-		if(currentGui != null) this.currentGui.draw(g2d);
+	public void draw(Graphics2D g2d) {
+		if (currentGui != null)
+			this.currentGui.draw(g2d);
 	}
 
 }
